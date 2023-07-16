ae1d5dd0a3fc09865d997ec406992fa531e7b3af
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-07-16 18:41:22 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,28 @@
-export const App = () => {
+import React from 'react';
+import Header from './Header/Header';
+import Nav from './Nav/Nav';
+import About from './About/About';
+import Experience from './Experience/Experience';
+import Suggestion from './Suggestion/Suggestion';
+import Portfolio from './Portfolio/Portfolio';
+import Testimonials from './Testimonials/Testimonials';
+import Contact from './Contact/Contact';
+import Footer from './Footer/Footer';
+
+const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      React homework template & hello Alekhandro!
-    </div>
+    <>
+      <Header />
+      <Nav />
+      <About />
+      <Experience />
+      <Suggestion />
+      <Portfolio />
+      <Testimonials />
+      <Contact />
+      <Footer />
+    </>
   );
 };
+
+export default App;
